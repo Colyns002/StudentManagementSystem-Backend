e5f79acd7b0618a3b8bab7c34b0861eaee6b3991
Colyns002o <colynsengorr2@gmail.com>
2026-03-18 12:08:37 +0300

    Complete CRUD implementation

--- src/StudentManagementAPI/StudentManagementAPI/Controllers/StudentsController.cs
+++ src/StudentManagementAPI/StudentManagementAPI/Controllers/StudentsController.cs
@@ -32,5 +32,49 @@ namespace StudentManagementAPI.Controllers
 
             return CreatedAtAction(nameof(GetStudents), new { id = student.StudentID }, student);
         }
+
+        // PUT: api/students/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStudent(int id, Student student)
+        {
+            if (id != student.StudentID)
+            {
+                return BadRequest("ID mismatch"); // Status 400
+            }
+
+            _context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Students.Any(e => e.StudentID == id))
+                {
+                    return NotFound(); // Status 404
+                }
+                throw;
+            }
+
+            return NoContent(); // Status 204
+        }
+
+        // DELETE: api/students/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound(); // Status 404
+            }
+
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return NoContent(); // Status 204
+        }
+
     }
 }
--- src/StudentManagementAPI/StudentManagementAPI/obj/Debug/net10.0/StudentManagementAPI.AssemblyInfo.cs
+++ src/StudentManagementAPI/StudentManagementAPI/obj/Debug/net10.0/StudentManagementAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7b6dad506732cda6c2eeb1faa74994dd9eb425d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f24ae88b7a340e7cf076b5b852509e90640c9b7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
